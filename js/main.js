0300e133e441b387b74736af147b3114a26cfd65
Mad Max <salikman@ukr.net>
2022-01-13 16:46:25 +0200

    add pages

--- js/main.js
+++ js/main.js
@@ -27,6 +27,22 @@
             }
         }
     })
+    $('.gallery-page-js').owlCarousel({
+        margin:30,
+        loop:true,
+        nav: true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                autoWidth:true,
+            }
+        }
+    })
     $('.exhibitions-js').owlCarousel({
         center: true,
         items:2,
@@ -41,4 +57,39 @@
             }
         }
     })
+    // swiper
+    let swiper = new Swiper(".mySwiper", {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesProgress: true,
+    });
+    let swiper2 = new Swiper(".mySwiper2", {
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        thumbs: {
+            swiper: swiper,
+        },
+    });
+
+    //BEGIN
+    $(".accordion__title").on("click", function(e) {
+
+        e.preventDefault();
+        let $this = $(this);
+
+        if (!$this.hasClass("accordion-active")) {
+            $(".accordion__content").slideUp(400);
+            $(".accordion__title").removeClass("accordion-active");
+            $('.accordion__arrow').removeClass('accordion__rotate');
+        }
+
+        $this.toggleClass("accordion-active");
+        $this.next().slideToggle();
+        $('.accordion__arrow',this).toggleClass('accordion__rotate');
+    });
+    //END
 })(jQuery)
